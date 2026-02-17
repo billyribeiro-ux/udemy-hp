13bbb97f62d9d0afa6a3c8bf9cb2ed124de5ba8f
Billy Ribeiro <billy.ribeiro@icloud.com>
2026-02-17 09:31:52 -0500

    .

--- src/lib/components/ChapterPreview.svelte
+++ src/lib/components/ChapterPreview.svelte
@@ -1,16 +1,16 @@
 <section class="chapter-preview default-margin">
-    <h2 class="mb-ml">What you're getting</h2>
-    <div class="chapter-conteiner">
+    <h2 class="mb-l">What you're getting</h2>
+    <div class="chapter-container">
         <ul>
             <li>
-            <button
-                class="chapter-title"
-                aria-controls="chapter-info-1"
+                <button
+                class="chapter0-title"
+                aria-controls="chapter-info 1"
                 aria-expanded="true"
-           >
-            <h3>Chapter 1: Whatever</h3>
-            </button>
-            </li>
-        </ul>
+            >
+            <h3> Chapter 1: Whatever</h3>
+            </button>   
+        </li>
+       </ul>
     </div>
 </section>
