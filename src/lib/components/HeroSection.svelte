66f1d92380e60b13ffea7beb1bef93d7b1477b29
Billy Ribeiro <billy.ribeiro@icloud.com>
2026-02-17 08:55:24 -0500

    Hero COmplete!

--- src/lib/components/HeroSection.svelte
+++ src/lib/components/HeroSection.svelte
@@ -2,6 +2,7 @@
 	//import bookCover from './assets/images/book_cover.png';
 	import bookCover from '$assets/book_cover.png';
 	import phoneCover from '$assets/phone_cover.png';
+	import { Button } from "$components";
 </script>
 
 <section class="hero">
@@ -16,34 +17,51 @@
 		<h1>THIS BOOK WILL</h1>
 		<h1>SAVE YOU MONEY,</h1>
 		<h1 class="mb-m">TIME & NERVES</h1>
-		<p class="light-grey">
+		<p class="light-grey mb-l" >
 			I moved to Spain seven years ago. The process was painful and difficult. learned most things
 			the hard way: finding a job, getting a visa, calculating my cost of living, choosing an
-			apartment. That's why i wrote this e-book, to tell you how I did it. No bullshit, no stress.<i
-				>TRUST ME</i
-			>
+			apartment. That's why i wrote this e-book, to tell you how I did it. No bullshit, no stress.
 		</p>
+		<Button> Purchase E-book for $10</Button>
 	</div>
 </section>
-
 <style>
-	.hero {
-		display: flex;
-		height: 100vh;
-		overflow-y: hidden;
-	}
+    .hero {
+        display: flex;
+        height: 100vh;
+        overflow-y: hidden;
+    }
 
-	.hero-showcase {
-		position: relative;
-		width: 50%;
-		padding: 100px 16px 0 16px;
-		background: linear-gradient(
-			135deg,
-			#e2bcbff,
-			#e2bcbff 25%,
-			#edeeaff 50%,
-			#edeeaff 75%,
-			#e927bce
-		);
-	}
+    .hero-showcase {
+        position: relative;
+        width: 50%;
+        padding: 100px 16px 0 16px;
+        background: linear-gradient(
+                135deg,
+                #e2bcbf,
+                #e2bcbf 25%,
+                #deeaff 50%,
+                #deeaff 75%,
+                #927bce
+        );
+    }
+    .hero-text {
+        padding: 100px 5%;
+        width: 50%;
+        background: black;
+    }
+    .book-cover {
+        position: absolute;
+        right: 2%;
+        bottom: 0;
+        width: 65%;
+        transform: translateY(30%);
+    }
+    .phone-cover {
+        position: absolute;
+        width: 35%;
+        bottom: 0;
+        right: 52%;
+        transform: translateY(25%);
+    }
 </style>
